e5ddc1b68e69171e9ff3836c15614112d0ebc5ea
lumeida <lumeida@localhost.localdomain>
2020-02-26 11:57:06 +0100

    Fin

--- app/src/main/java/com/example/exo/Main4Activity.java
+++ app/src/main/java/com/example/exo/Main4Activity.java
@@ -1,9 +1,16 @@
 package com.example.exo;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,19 +25,43 @@ public class Main4Activity extends AppCompatActivity {
         if (bundle != null) {
             TextView fn = (TextView) findViewById(R.id.txt_user_fn);
             TextView ln = (TextView) findViewById(R.id.user_ln);
-            TextView tel = (TextView) findViewById(R.id.user_phone);
+            final TextView tel = (TextView) findViewById(R.id.user_phone);
             TextView ugender = (TextView) findViewById(R.id.user_gender);
+            Button btn = (Button) findViewById(R.id.btn_call);
 
-            String first_name =  bundle.getString("user_first_name");
-            String last_name =  bundle.getString("user_last_name");
-            String gender =  bundle.getString("user_sexe");
-            String phone =  bundle.getString("user_phone");
+            String first_name = bundle.getString("user_first_name");
+            String last_name = bundle.getString("user_last_name");
+            String gender = bundle.getString("user_sexe");
+            String phone = bundle.getString("user_phone");
 
             fn.setText(first_name);
             ln.setText(last_name);
             tel.setText(phone);
             ugender.setText(gender);
 
+            btn.setOnClickListener(new View.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onClick(View v) {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:96346856"));
+                    if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    Activity#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for Activity#requestPermissions for more details.
+                        return;
+                    }
+                    startActivity(callIntent);
+                }
+            });
+
+
         }
     }
+
+
 }
